3c8c1eab4b5dd91bdc66251c2a783064b4247f2d
lukadlm97@gmail.com <lukadlm97@gmail.com>
2023-04-09 17:20:43 +0200

    Implementation - Added basic unit tests to cover shared application layer

---
+++ Homework.Enigmatry.Application.Shared.UnitTests/Features/Article/GetArticleByIdTests.cs
@@ -0,0 +1,61 @@
+﻿using AutoMapper;
+using FluentAssertions;
+using Homework.Enigmatry.Application.Shared.Contracts;
+using Homework.Enigmatry.Application.Shared.Features.Articles.Handlers.Queries;
+using Homework.Enigmatry.Application.Shared.Features.Articles.Requests.Queries;
+using Homework.Enigmatry.Application.Shared.Profiles;
+using Homework.Enigmatry.Application.Shared.UnitTests.Mocks;
+using Homework.Enigmatry.Logging.Shared.Contracts;
+using Homework.Enigmatry.Shop.Domain.Enums;
+using Moq;
+
+namespace Homework.Enigmatry.Application.Shared.UnitTests.Features.Article
+{
+    public class GetArticleByIdHandlerTests
+    {
+        private readonly IMapper _mapper;
+        private readonly Mock<IArticleRepository> _articleRepositoryMock;
+
+        public GetArticleByIdHandlerTests()
+        {
+            _articleRepositoryMock = MockArticleRepository.GetArticleRepository();
+
+            var mapperConfig = new MapperConfiguration(c =>
+            {
+                c.AddProfile<MappingProfile>();
+            });
+
+            _mapper = mapperConfig.CreateMapper();
+        }
+
+        [Xunit.Theory]
+        [InlineData(1)]
+        [InlineData(2)]
+        [InlineData(3)]
+        public async Task GetArticleById_ShouldReturnArticle(int id)
+        {
+            var handler = new GetArticleByIdHandler(_articleRepositoryMock.Object,_mapper,new LogTraceData());
+
+            var result = await handler.Handle(new GetArticleByIdRequest(){Id = id}, CancellationToken.None);
+
+            result.Status.Should().Be(OperationStatus.Success);
+            result.Result.Name.Should().Be(string.Format("article {0}",id));
+        }
+
+        [Xunit.Theory]
+        [InlineData(-1)]
+        [InlineData(0)]
+        [InlineData(10)]
+        public async Task GetArticleById_ShouldNotReturnArticle(int id)
+        {
+            var handler = new GetArticleByIdHandler(_articleRepositoryMock.Object, _mapper, new LogTraceData());
+
+            var result = await handler.Handle(new GetArticleByIdRequest() { Id = id }, CancellationToken.None);
+
+            result.Status.Should().Be(OperationStatus.NotFound);
+        }
+
+
+
+    }
+}
---
+++ Homework.Enigmatry.Application.Shared.UnitTests/Features/Article/IsArticleExistTests.cs
@@ -0,0 +1,63 @@
+﻿using AutoMapper;
+using Homework.Enigmatry.Application.Shared.Contracts;
+using Homework.Enigmatry.Application.Shared.Features.Articles.Handlers.Queries;
+using Homework.Enigmatry.Application.Shared.Features.Articles.Requests.Queries;
+using Homework.Enigmatry.Application.Shared.UnitTests.Mocks;
+using Homework.Enigmatry.Logging.Shared.Contracts;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Homework.Enigmatry.Application.Shared.Profiles;
+using Homework.Enigmatry.Shop.Application.Features.Articles.Handlers.Queries;
+using Homework.Enigmatry.Shop.Domain.Enums;
+
+namespace Homework.Enigmatry.Application.Shared.UnitTests.Features.Article
+{
+    public class IsArticleExistTests
+    {
+        private readonly IMapper _mapper;
+        private readonly Mock<IArticleRepository> _articleRepositoryMock;
+
+        public IsArticleExistTests()
+        {
+            _articleRepositoryMock = MockArticleRepository.GetArticleRepository();
+
+            var mapperConfig = new MapperConfiguration(c =>
+            {
+                c.AddProfile<MappingProfile>();
+            });
+
+            _mapper = mapperConfig.CreateMapper();
+        }
+
+        [Xunit.Theory]
+        [InlineData(1)]
+        [InlineData(2)]
+        [InlineData(3)]
+        public async Task IsArticleExist_ShouldReturnTrue(int id)
+        {
+            var handler = new IsArticleExistHandler(_articleRepositoryMock.Object, new LogTraceData());
+
+            var result = await handler.Handle(new IsArticleExistRequest() { Id = id }, CancellationToken.None);
+
+            result.Should().BeTrue();
+        }
+
+        [Xunit.Theory]
+        [InlineData(-1)]
+        [InlineData(0)]
+        [InlineData(10)]
+        public async Task IsArticleExist_ShouldReturnFalse(int id)
+        {
+            var handler = new IsArticleExistHandler(_articleRepositoryMock.Object, new LogTraceData());
+
+            var result = await handler.Handle(new IsArticleExistRequest() { Id = id }, CancellationToken.None);
+
+            result.Should().BeFalse();
+        }
+    }
+}
